217d6fd44f9ef68690d5793a859df090942a165c
AnoraDragoon <anoradragoon@gmail.com>
2023-08-18 18:18:30 +0200

    Generate pages and routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { PodcastDetailsComponent } from './pages/podcast-details/podcast-details.component';
+import { EpisodeDetailsComponent } from './pages/episode-details/episode-details.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent
+    },
+    {
+        path: 'podcast/podcastId/episode',
+        component: EpisodeDetailsComponent
+    },
+    {
+        path: 'podcast',
+        component: PodcastDetailsComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { PodcastDetailsComponent } from './pages/podcast-details/podcast-details.component';
+import { EpisodeDetailsComponent } from './pages/episode-details/episode-details.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PodcastDetailsComponent,
+    EpisodeDetailsComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
